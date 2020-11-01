166f9fde6b8ed0ac427a7ef86336b0e11b955da3
yekogarcia@yahoo.com <joya.d.f.o2017@gmail.com>
2020-10-31 20:50:54 -0500

    pantallas de inicio

--- views/modules/inicio.php
+++ views/modules/inicio.php
@@ -3,6 +3,20 @@
           <img src="views/imagenes/inicio.jpg" alt="">
           <div class="contenedor">
           <h1>BIENVENIDOS A MI CAMPO</h1> 
-              <P>¿Quieres hacer parte de esta gran comunidad?</P>
+          <P class="parrafo1">¿Quieres hacer parte de esta gran comunidad?</P>
           </div>
       </section>
+      <article class="contenerdor2">
+
+      <h2 class="subtitulos">EN QUE CONSISTE</h2>
+        <P>Es una plataforma web para <strong> la comercialización de productos campesinos</strong>, de  esta forma podrán vender sus productos con mayor  facilidad y sin necesidad de intermediarios.
+Atreves de esta plataforma se verán beneﬁciados  tanto productores, comerciantes y transportadores de  alimentos ya que los campesinos podrán ofrecer sus  productos a cualquier parte del país y cualquier  comerciante podrá ver los productos que están  disponibles en el momento y hacer sus pedidos si lo  desean.
+</P>
+
+<h3 class="subtitulos">¿COMO REGISTRARSE?</h3>
+  <P class="parrafo2">se puede registrar bajo tres perfiles:  </P>
+  <div class="centrado"><img  src="views/imagenes/perfiles.jpg" alt=""></div>
+  <p class="parrafo2"><strong> Productores </strong>: pueden registrar dueños de fincas que esten interesados en vender sus productos.</p>  
+  <p class="parrafo2"><strong>Comerciantes </strong>: bajo este perfil se registran dueños de negocios que esten interesados en adquirir productos campesinos con una mayor facilidad ya que estan negociando directamente con los productores.</p>  
+  <p class="parrafo2"><strong>Transportadores</strong>:se registran dueños de vehiculos que esten interesados en transportar los productos a los diferentes puntos acordados para la entrega. </p>
+      </article>
--- views/modules/somos.php
+++ views/modules/somos.php
@@ -1,1 +1,15 @@
-<h1>pagina de somos</h1>
+<section id="banner">
+          <img src="views/imagenes/somos.jpg" alt="">
+          <div class="contenedor">
+          <h1>SOMOS TU APOYO </h1> 
+          <P class="parrafo1">Te ayudamos en la venta de tus productos</P>
+          </div>
+      </section>
+
+        <article class="contenerdor2">
+            <p>Somos un grupo de campesinos, comerciantes y transportadores en búsqueda de un comercio mas justo y competitivo con igualdad de condiciones para todos.
+A través de esta plataforma se busca beneficiar tanto a campesinos como a comerciantes, ya que ellos tendrán un mayor facilidad para poder vender sus productos sin necesidad de intermediarios y los comerciantes podrán surtir sus negocios comprando directamente a los campesinos.
+</p>
+<h3></h3>
+
+        </article>
