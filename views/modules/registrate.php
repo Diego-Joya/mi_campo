b649ebb03e2905a36ff6900a6468d78893602d94
yeko <yekogarcia@yahoo.com>
2020-11-02 20:19:29 -0500

    cambios en registrarse

--- views/modules/registrate.php
+++ views/modules/registrate.php
@@ -1,17 +1,28 @@
 <div class="register">
     <form id="registro" action="" method="POST">
         <h1>Registrese</h1>
-
-        <label for="">Nombres</label><br>
-        <input type="text" name="nombres" id=""><br><br>
-        <label for="">Apellidos</label><br>
-        <input type="text" name="apellidos" id=""><br><br>
-        <label for="">N° Documento</label><br>
-        <input type="text" name="documento" id=""><br><br>
-        <label for="">Celular</label><br>
-        <input type="text" name="celular" id=""><br><br>
-        <label for="">Email</label><br>
-        <input type="text" name="email" id=""><br>
+        <div class="form_registro">
+            <div class="information_user">
+                <label for="">Nombres</label><br>
+                <input type="text" name="nombres" id=""><br><br>
+                <label for="">Apellidos</label><br>
+                <input type="text" name="apellidos" id=""><br><br>
+                <label for="">N° Documento</label><br>
+                <input type="text" name="documento" id=""><br><br>
+                <label for="">Celular</label><br>
+                <input type="text" name="celular" id=""><br><br>
+                <label for="">Email</label><br>
+                <input type="text" name="email" id=""><br>
+            </div>
+            <div class="security_user">
+                <label for="">Usuario</label><br>
+                <input type="text" name="usuario" id=""><br>
+                <label for="">Contraseña</label><br>
+                <input type="text" name="password" id=""><br>
+                <label for="">Confirmar contraseña</label><br>
+                <input type="text" name="repeat_password    " id=""><br>
+            </div>
+        </div>
         <input type="button" id="register" value="Registrarme">
     </form>
 </div>
