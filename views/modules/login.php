59bad84cc9682872a52e88bc5c37612b983ef80f
yekogarcia@yahoo.com <joya.d.f.o2017@gmail.com>
2020-11-09 21:27:19 -0500

    cambios ingreso

--- views/modules/login.php
+++ views/modules/login.php
@@ -1,11 +1,20 @@
 <div class="login">
     <h2>INGRESO A MI CAMPO</h2>
     <form action="">
-        <label for="">Usuario</label><br>
-        <input type="text" name="usuario" id="" placeholder="&#128100; escriba su usuario" require><br><br>
-        <label for="">Contraseña</label><br>
-        <input type="password" name="password" id="" placeholder="&#128272; escriba su contraseña" require><br><br>
-        <input type="submit" id="register" value="INGRESAR">
+        <label for="">Perfil</label><br>
+        
+           <i class="material-icons">list</i>
+            <select name="perfil" id="perfil" form="perfil" require>
+                <option value="">Seleccione</option>
+                <option value="Productor">Productor</option>
+                <option value="Comerciante">Comerciante</option>
+                <option value="Transportador">Transportador</option>
+            </select>
+            <label for="">Usuario</label><br>
+            <input type="text" name="usuario" id="" placeholder="&#128100; escriba su usuario" require><br><br>
+            <label for="">Contraseña</label><br>
+            <input type="password" name="password" id="" placeholder="&#128272; escriba su contraseña" require><br><br>
+            <input type="submit" id="register" value="INGRESAR">
 
     </form>
 </div>
