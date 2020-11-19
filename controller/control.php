8b78b84dd4b463270103e3e6ba7a262ba5f711b2
yeko <yekogarcia@yahoo.com>
2020-11-19 13:16:54 -0500

    cambios en  el model

--- controller/control.php
+++ controller/control.php
@@ -1,49 +1,20 @@
 <?php
+class controller
+{
 
-class controller{
-
-    public function plantilla(){
+    public function plantilla()
+    {
         include "views/plantilla.php";
     }
 
-public function enlacespaginascontroller(){
-    if ( isset( $_GET["acction"])){
-        $enlacescontroller = $_GET["acction"];
+    public function enlacespaginascontroller()
+    {
+        if (isset($_GET["acction"])) {
+            $enlacescontroller = $_GET["acction"];
+        } else {
+            $enlacescontroller = "index.php";
+        }
+        $respuesta = enlacespaginas::enlacespaginasmodel($enlacescontroller);
+        include $respuesta;
     }
-  else {
-      $enlacescontroller = "index.php";
-  }
-     $respuesta = enlacespaginas :: enlacespaginasmodel($enlacescontroller);
-     include $respuesta;
-
 }
-
-public function entrar(){
-    if(isset($_POST["perfil"])){
-                        $datoscontroller= array("perfil"=>$_POST["perfil"],
-                        "usuario"=>$_POST["usuario"],
-                        "password"=>$_POST["password"]  );}
-                        
-    $respu=Singup::entrarmodel($datoscontroller,"usuarios");
-    
-    if($respu["perfil"]==$_POST["perfil"]&&$respu["usuario"]==$_POST["usuario"]
-                        &&$respu["password"]==$_POST["password"]){
-                            header("location:productores.php");
-                        }
-                        else{
-                            header("location:fallo.php");
-
-                        }
-                        
-    }
-     
-}
-
-
-
-
-
-
-
-
-?>
