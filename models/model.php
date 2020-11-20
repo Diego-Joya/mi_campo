16b61dd17128bb1da54e3fab433f4e1d12a4a35f
yeko <yekogarcia@yahoo.com>
2020-11-20 14:04:26 -0500

    validación de plantilla

--- models/model.php
+++ models/model.php
@@ -1,73 +1,67 @@
-<?php 
+<?php
 require_once "connection.php";
- class enlacespaginas extends connection{
- public function enlacespaginasmodel($enlacesmodel){
-     if($enlacesmodel== "contacto"||
-        $enlacesmodel == "login"||
-        $enlacesmodel == "registrate"||
-        $enlacesmodel == "services" ||
-        $enlacesmodel == "somos"){
-        
-            $module = "views/modules/".$enlacesmodel.".php";
-        }
-        else if( $enlacesmodel == "index.php"){ 
-            $module = "views/modules/inicio.php";
-        }
-        else if( $enlacesmodel == "inicio"){ 
-            $module = "views/modules/inicio.php";
-        }
-        else if( $enlacesmodel == "administracion"){ 
-            $module = "views/administracion.php";
-        }
-        else if( $enlacesmodel == "fallo"){ 
-            $module = "views/modules/login.php";
-         
-        }
-        else{
-            $module = "views/modules/inicio.php";
-        }
-     
-     return$module;
- }
-
- public function enlacespaginascontroller()
- {
-     if (isset($_GET["acction"])) {
-         $enlacescontroller = $_GET["acction"];
-     } else {
-         $enlacescontroller = "index.php";
-     }
-     $respuesta = enlacespaginas::enlacespaginasmodel($enlacescontroller);
-     include $respuesta;
- }
-
- // enlaces paginas administracion
-
-   public function enlacesadministracionmodel($enlacesmode)
+class enlacespaginas extends connection
+{
+    public function enlacespaginasmodel($enlacesmodel)
     {
-        if ( $enlacesmode=="pag-transportadores"||
-                 $enlacesmode=="pag-comerciantes"|| $enlacesmode == "ver-productos"|| 
-                 $enlacesmode== "ventas"|| $enlacesmode=="ver-productos"|| 
-                 $enlacesmode=="comprar-productos") {
-                     $modulo= "views/modules/".$enlacesmode.".php";
-       
+        if (
+            $enlacesmodel == "contacto" ||
+            $enlacesmodel == "login" ||
+            $enlacesmodel == "registrate" ||
+            $enlacesmodel == "services" ||
+            $enlacesmodel == "somos"
+        ) {
+
+            $module = "views/modules/" . $enlacesmodel . ".php";
+        } else if ($enlacesmodel == "index.php") {
+            $module = "views/modules/inicio.php";
+        } else if ($enlacesmodel == "inicio") {
+            $module = "views/modules/inicio.php";
+        } else if ($enlacesmodel == "administracion") {
+            $module = "views/administracion.php";
+        } else if ($enlacesmodel == "fallo") {
+            $module = "views/modules/login.php";
+        } else {
+            $module = "views/modules/inicio.php";
+        }
+
+        return $module;
     }
-    else if($enlacesmode== "administracion.php"){
-        $modulo= "views/modules/pag-productores.php";
+
+    public function enlacespaginascontroller()
+    {
+        if (isset($_GET["acction"])) {
+            $enlacescontroller = $_GET["acction"];
+        } else {
+            $enlacescontroller = "index.php";
+        }
+        $respuesta = enlacespaginas::enlacespaginasmodel($enlacescontroller);
+        include $respuesta;
     }
-    return $modulo;
+
+    // enlaces paginas administracion
+
+    public function enlacesadministracionmodel($enlacesmode)
+    {
+        if (
+            $enlacesmode == "pag-transportadores" ||
+            $enlacesmode == "pag-comerciantes" || $enlacesmode == "ver-productos" ||
+            $enlacesmode == "ventas" || $enlacesmode == "ver-productos" ||
+            $enlacesmode == "comprar-productos"
+        ) {
+            $modulo = "views/modules/" . $enlacesmode . ".php";
+        } else if ($enlacesmode == "administracion.php") {
+            $modulo = "views/modules/pag-productores.php";
+        }
+        return $modulo;
+    }
+
+
+    // llamar productos de la bbdd
+    public function cultivosmodel($tabla)
+    {
+        $prod = connection::connect()->prepare("SELECT id_producto,nomb_producto,tipo_producto FROM $tabla");
+        $prod->execute();
+        return $prod->fetchAll();
+    }
 }
-
-
- // llamar productos de la bbdd
- public function cultivosmodel($tabla){
-    $prod = connection::connect()->prepare("SELECT id_producto,nomb_producto,tipo_producto FROM $tabla");
-$prod->execute();
-return $prod ->fetchAll();
-}
-
-
- }
-
-
-?>
