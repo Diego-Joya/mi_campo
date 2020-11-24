67caed149cfa7da0d3641f542dd304aac807c3ce
yeko <yekogarcia@yahoo.com>
2020-11-23 22:43:32 -0500

    assa

--- views/js/index.js
+++ views/js/index.js
@@ -1,15 +1,19 @@
 $("#close").click(function () {
+  var exist = document.querySelector(".pupup");
   var close = document.createElement("div");
   close.className = "pupup";
   close.innerHTML =
     "<div class='close'><p>¿Estas seguro de cerrar sessión?</p><br> <div class='buttons'><button  id='close_cancel'>Cancelar</button><button id='close_accept'>Aceptar</button></div></div>";
   var body = document.querySelector(".contendenor3");
-  body.append(close);
+  if(exist === null){
+    body.append(close);
+  }
 });
 $(".contendenor3").delegate("#close_cancel", "click", function () {
   var d = document.querySelector(".pupup");
   d.remove();
 });
+
 $(".contendenor3").delegate("#close_accept", "click", function () {
   $.ajax({
     type: "POST",
